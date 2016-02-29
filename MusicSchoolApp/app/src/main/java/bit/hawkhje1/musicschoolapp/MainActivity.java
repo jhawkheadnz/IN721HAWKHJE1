8262f785802ec28d91232ebee00ab1fd632eb123
Joshua Hawkhead <joshua.hawkhead@gmail.com>
2016-03-01 02:20:42 +1300

    Updated resources to remove literals

--- MusicSchoolApp/app/src/main/java/bit/hawkhje1/musicschoolapp/MainActivity.java
+++ MusicSchoolApp/app/src/main/java/bit/hawkhje1/musicschoolapp/MainActivity.java
@@ -3,7 +3,6 @@ package bit.hawkhje1.musicschoolapp;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.Adapter;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.RadioButton;
