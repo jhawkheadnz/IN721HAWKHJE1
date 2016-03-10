e2a7cac12a44341e60232eb414f9598204364187
Joshua Hawkhead <joshua.hawkhead@gmail.com>
2016-03-11 00:53:16 +1300

    All About Dunedin App with Drawer Navigation

---
+++ AllAboutDunedinDrawer/app/src/main/java/bit/hawkhje1/allaboutdunedindrawer/ActivitiesActivity.java
@@ -0,0 +1,27 @@
+package bit.hawkhje1.allaboutdunedindrawer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class ActivitiesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_subscreen_layout);
+        loadPageContent();
+    }
+
+    private void loadPageContent()
+    {
+        TextView headerText = (TextView)findViewById(R.id.txtSubCategoryTitle);
+        ImageView headerImage = (ImageView)findViewById(R.id.imgHeader);
+
+        // Load subcategory heading
+        String categoryTitle = getResources().getString(R.string.title_activities);
+        headerText.setText(categoryTitle);
+        headerImage.setImageResource(R.drawable.big_octagon);
+    }
+}
---
+++ AllAboutDunedinDrawer/app/src/main/java/bit/hawkhje1/allaboutdunedindrawer/MainActivity.java
@@ -0,0 +1,99 @@
+package bit.hawkhje1.allaboutdunedindrawer;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    // http://developer.android.com/training/basics/firstapp/starting-activity.html
+    public final static String EXTRA_TITLE = "bit.hawkhje1.allaboutdunedinlist.EXTRA_TITLE";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //getActionBar().setDisplayHomeAsUpEnabled(true);
+
+        // load all about dunedin content
+        loadContent();
+
+        // load navigation list
+        setupNavigationList();
+
+        // create and attach navigation click listener to listview
+        NavigationClickListener navigationClickListener = new NavigationClickListener();
+        ListView lvNavigation = (ListView)findViewById(R.id.left_drawer);
+        lvNavigation.setOnItemClickListener(navigationClickListener);
+    }
+
+    private void loadContent()
+    {
+        String pageTitle = getResources().getString(R.string.main_title);
+
+        TextView txtTitle = (TextView)findViewById(R.id.txtTitle);
+        txtTitle.setText(pageTitle);
+    }
+
+    /**
+     * Setup navigation list for all about dunedin navigation
+     */
+    private void setupNavigationList() {
+        // load navigation links from strings resource file
+        String[] navigationLinks = getResources().getStringArray(R.array.navigation_text);
+
+        // place items in navigation links into array adaptor
+        ArrayAdapter<String> navLinks = new ArrayAdapter<String>(MainActivity.this,android.R.layout.simple_list_item_1, navigationLinks);
+
+        // place items into listview
+        ListView lvNavigation = (ListView)findViewById(R.id.left_drawer);
+        lvNavigation.setAdapter(navLinks);
+
+
+    }
+
+    /**
+     * Navigation Click Listener for Main Menu
+     */
+    class NavigationClickListener implements AdapterView.OnItemClickListener
+    {
+
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+            // create intent for sub screen activity
+            Intent categoryIntent = null;
+
+            // get the category from the selected item
+            String category = parent.getItemAtPosition(position).toString().toLowerCase();
+
+            // select the intent to load based on the category
+            switch(category)
+            {
+                case "activities":
+                    categoryIntent = new Intent(MainActivity.this, ActivitiesActivity.class);
+                    break;
+                case "shopping":
+                    categoryIntent = new Intent(MainActivity.this, ShoppingActivity.class);
+                    break;
+                case "dining":
+                    categoryIntent = new Intent(MainActivity.this, DiningActivity.class);
+                    break;
+                case "services":
+                    categoryIntent = new Intent(MainActivity.this, ServicesActivity.class);
+                    break;
+            }
+
+            // start selected activity
+            if(categoryIntent != null)
+                startActivity(categoryIntent);
+
+        }
+    }
+
+}
