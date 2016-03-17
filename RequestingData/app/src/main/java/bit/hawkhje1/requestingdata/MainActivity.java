1e185437c3985055e64e4c9ff16a6becad2da064
Joshua Hawkhead <joshua.hawkhead@gmail.com>
2016-03-18 00:37:11 +1300

    Setting up RequestingData xml

---
+++ RequestingData/app/src/main/java/bit/hawkhje1/requestingdata/MainActivity.java
@@ -0,0 +1,25 @@
+package bit.hawkhje1.requestingdata;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        //super.onActivityResult(requestCode, resultCode, data);
+
+
+
+    }
+}
---
+++ RequestingData/app/src/main/java/bit/hawkhje1/requestingdata/SettingsActivity.java
@@ -0,0 +1,13 @@
+package bit.hawkhje1.requestingdata;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+    }
+}
