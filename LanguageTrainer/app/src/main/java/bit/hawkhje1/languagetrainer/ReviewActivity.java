c01b4eaf9ddfd1cc77a169f75f4b46aee68a3dad
Joshua Hawkhead <joshua.hawkhead@gmail.com>
2016-03-28 02:03:23 +1300

    Created Review Manager

--- LanguageTrainer/app/src/main/java/bit/hawkhje1/languagetrainer/ReviewActivity.java
+++ LanguageTrainer/app/src/main/java/bit/hawkhje1/languagetrainer/ReviewActivity.java
@@ -3,18 +3,14 @@ package bit.hawkhje1.languagetrainer;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
 import android.widget.Button;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import java.util.List;
 
 import bit.hawkhje1.languagetrainer.Classes.AnswerWord;
-import bit.hawkhje1.languagetrainer.Classes.ResultsAdapter;
-import bit.hawkhje1.languagetrainer.Classes.Word;
-import bit.hawkhje1.languagetrainer.Enums.Article;
+import bit.hawkhje1.languagetrainer.Managers.ReviewManager;
 
 public class ReviewActivity extends AppCompatActivity {
 
@@ -29,55 +25,16 @@ public class ReviewActivity extends AppCompatActivity {
         Intent intent = getIntent();
 
         // get the list view
-        ListView resutlsList = (ListView)findViewById(R.id.lvResults);
+        ListView resultsList = (ListView)findViewById(R.id.lvResults);
         TextView txtResultScore = (TextView)findViewById(R.id.txtResultScore);
+        Button btnHome = (Button)findViewById(R.id.btnHome);
 
         // get the answers from intent
         userAnswers = intent.getParcelableArrayListExtra(PracticeActivity.RESULT_LIST_ID);
 
-        int questionsCount = userAnswers.size();
-
-        int questionsCorrect = 0;
-        float percentage;
-
-        for(AnswerWord answerWord : userAnswers){
-
-            Word word = answerWord.getWord();
-            Article article = answerWord.getArticleAnswer();
-
-            if(word.containsArticle(article)) {
-                questionsCorrect++;
-            }
-
-        }
-
-        percentage =(((float)questionsCorrect / (float)questionsCount) * 100);
-
-        txtResultScore.setText(String.format("%s/%s - %.2f%%", Integer.toString(questionsCorrect), Integer.toString(questionsCount), percentage));
-
-        // create an array adapter for List view to show results to user
-        //ArrayAdapter<AnswerWord> answerWordArrayAdapter = new ArrayAdapter<>(ReviewActivity.this,android.R.layout.simple_list_item_1, userAnswers);
-        ResultsAdapter resultsAdapter = new ResultsAdapter(ReviewActivity.this, android.R.layout.simple_list_item_1, userAnswers);
-
-        // set the list view's adapter
-        resutlsList.setAdapter(resultsAdapter);
-
-        // home button
-        Button btnHome = (Button)findViewById(R.id.btnHome);
-        BtnHomeHandler btnHomeHandler = new BtnHomeHandler();
-        btnHome.setOnClickListener(btnHomeHandler);
+        ReviewManager.LoadReviewManager(ReviewActivity.this, userAnswers, btnHome, txtResultScore, resultsList);
 
     }
 
-    /**
-     * Send the user back to the home screen
-     */
-    private class BtnHomeHandler implements View.OnClickListener{
 
-        @Override
-        public void onClick(View v) {
-            Intent homeIntent = new Intent(ReviewActivity.this, MainActivity.class);
-            startActivity(homeIntent);
-        }
-    }
 }
