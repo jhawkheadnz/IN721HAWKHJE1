e4afcd270ccc975510c87f0b7a4ea7909fc0bcff
Joshua Hawkhead <joshua.hawkhead@gmail.com>
2016-04-08 08:15:23 +1200

    Updated MainActivity

--- ExternalDataSQLite2/app/src/main/java/bit/hawkhje1/externaldatasqlite2/MainActivity.java
+++ ExternalDataSQLite2/app/src/main/java/bit/hawkhje1/externaldatasqlite2/MainActivity.java
@@ -4,14 +4,19 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Button;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
+    private CitiesDBManager citiesDBManager;
     private String DB_NAME = "dbPlaces";
 
     @Override
@@ -22,61 +27,45 @@ public class MainActivity extends AppCompatActivity {
         // create database
         SQLiteDatabase dbCountries = openOrCreateDatabase(DB_NAME, MODE_PRIVATE, null);
 
-        // create table
-        String createTblCities = "CREATE TABLE tblCities (" +
-                "    cityID INTEGER PRIMARY KEY AUTOINCREMENT, " +
-                "    cityName TEXT NOT NULL, " +
-                "    countryName TEXT NOT NULL);";
+        // create tblCities if it doesn't exist
+        citiesDBManager = new CitiesDBManager(dbCountries);
+        citiesDBManager.createTblCities();
 
-        // execute sql query
-        dbCountries.execSQL(createTblCities);
+        Button btnPopulateTable = (Button)findViewById(R.id.btnPopulateTable);
+        BtnPopulateTableHandler btnPopulateTableHandler = new BtnPopulateTableHandler();
+        btnPopulateTable.setOnClickListener(btnPopulateTableHandler);
 
-        // populate table
-        List<City> cities = new ArrayList<>();
-        cities.add(new City("Amsterdam", "The Netherlands"));
-        cities.add(new City("Berlin", "Germany"));
-        cities.add(new City("Cairo", "Egypt"));
-        cities.add(new City("Dunedin", "New Zealand"));
+        // get a list of items from the database
+        List<City> citiesFromTblCities = citiesDBManager.GetCities();
 
-        // formatted string
-        String sqlFormat = "INSERT INTO (cityName, countryName) VALUES ('%s', '%s')";
-
-        for(City city : cities){
-            String sql = String.format(sqlFormat, city.getName(), city.getCountry());
-            dbCountries.execSQL(sql);
-        }
-
-        // display in list view
-        String selectAll = "SELECT cityName, countryName FROM tblCities";
-        Cursor recordSet = dbCountries.rawQuery(selectAll, null);
-
-        int recordCount = recordSet.getCount();
-
-        String[] citiesStringArray = new String[recordCount];
-
-        int cityNameIndex = recordSet.getColumnIndex("cityName");
-        int countryNameIndex = recordSet.getColumnIndex("countryName");
-
-        // move to the first record
-        recordSet.moveToFirst();
-
-        for(int i = 0; i < recordCount; i++){
-            String cityName = recordSet.getString(cityNameIndex);
-            String countryName = recordSet.getString(countryNameIndex);
-            citiesStringArray[i] = String.format("%s, %s", cityName, countryName);
-            recordSet.moveToNext();
-        }
-        recordSet.close();
-
-        ArrayAdapter<String> citiesAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, citiesStringArray);
+        ArrayAdapter<City> citiesAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, citiesFromTblCities);
         ListView lvCities = (ListView)findViewById(R.id.lvCities);
         lvCities.setAdapter(citiesAdapter);
     }
 
+    private List<City> getCitiesList(){
+
+        List<City> cities = new ArrayList<>();
+
+        cities.add(new City("Amsterdam", "The Netherlands"));
+        cities.add(new City("Berlin", "Germany"));
+        cities.add(new City("Cairo", "Egypt"));
+        cities.add(new City("Dunedin", "New Zealand"));
+        cities.add(new City("Auckland", "New Zealand"));
+        cities.add(new City("Wellington", "New Zealand"));
+        cities.add(new City("Christchurch", "New Zealand"));
+        cities.add(new City("Sydney", "Australia"));
+        cities.add(new City("Melbourne", "Australia"));
+        cities.add(new City("Adalade", "Australia"));
+        cities.add(new City("Brisbane", "Australia"));
+
+        return cities;
+    }
+
     private class City {
 
+        private int id;
         private String name;
-
         private String country;
 
         public City(String name, String country){
@@ -84,9 +73,104 @@ public class MainActivity extends AppCompatActivity {
             this.country = country;
         }
 
-        public String getName(){ return this.name; }
+        public City(int id, String name, String country){
+            this(name, country);
+            this.id = id;
+        }
 
+        public int getId(){ return this.id; }
+        public String getName(){ return this.name; }
         public String getCountry(){ return this.country; }
+
+        @Override
+        public String toString(){ return String.format("%s, %s", name, country); }
     }
 
+    private class CitiesDBManager {
+
+        private SQLiteDatabase sqLiteDatabase;
+
+        public CitiesDBManager(SQLiteDatabase sqLiteDatabase){
+            this.sqLiteDatabase = sqLiteDatabase;
+        }
+
+        public void createTblCities(){
+            // create table
+            String createTblCities = "CREATE TABLE IF NOT EXISTS tblCities (" +
+                    "    cityID INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                    "    cityName TEXT NOT NULL, " +
+                    "    countryName TEXT NOT NULL);";
+
+            // execute sql query
+            this.sqLiteDatabase.execSQL(createTblCities);
+        }
+
+        public void addCityToTblCities(City city){
+
+            String sqlFormat = "INSERT INTO tblCities (cityName, countryName) VALUES('%s', '%s');";
+            String sql = String.format(sqlFormat, city.getName(), city.getCountry());
+
+            // execute the sql query
+            this.sqLiteDatabase.execSQL(sql);
+
+        }
+
+        public void addCitiesToTblCities(List<City> cities){
+
+            // go through each city and add city to table
+            for(City city : cities){
+                addCityToTblCities(city);
+            }
+
+        }
+
+        public List<City> GetCities() {
+
+            // display in list view
+            String selectAll = "SELECT cityID, cityName, countryName FROM tblCities";
+            Cursor recordSet = this.sqLiteDatabase.rawQuery(selectAll, null);
+
+            int recordCount = recordSet.getCount();
+
+            City[] citiesStringArray = new City[recordCount];
+
+            int idIndex = recordSet.getColumnIndex("cityID");
+            int cityNameIndex = recordSet.getColumnIndex("cityName");
+            int countryNameIndex = recordSet.getColumnIndex("countryName");
+
+            // move to the first record
+            recordSet.moveToFirst();
+
+            for(int i = 0; i < recordCount; i++){
+
+                int id = recordSet.getInt(idIndex);
+                String cityName = recordSet.getString(cityNameIndex);
+                String countryName = recordSet.getString(countryNameIndex);
+
+                citiesStringArray[i] = new City(id, cityName, countryName);
+
+                // move to the next item in the recordset
+                recordSet.moveToNext();
+            }
+
+            // close record set
+            recordSet.close();
+
+            // return list of items
+            return Arrays.asList(citiesStringArray);
+        }
+    }
+
+    private class BtnPopulateTableHandler implements ListView.OnClickListener {
+        @Override
+        public void onClick(View v) {
+            Button btnPopulateTable = (Button)findViewById(R.id.btnPopulateTable);
+
+            // populate tbl cities
+            List<City> cities = getCitiesList();
+            citiesDBManager.addCitiesToTblCities(cities);
+
+            btnPopulateTable.setVisibility(View.INVISIBLE);
+        }
+    }
 }
