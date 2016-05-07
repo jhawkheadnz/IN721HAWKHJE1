49fd61b9bf835671cd7321eb558948b775fa462b
Joshua Hawkhead <joshua.hawkhead@gmail.com>
2016-05-08 00:48:47 +1200

    Added GeoPlugin

---
+++ LocationTeleporterRand/app/src/main/java/bit/hawkhje1/locationteleporterrand/GeoPluginMaxIterationsException.java
@@ -0,0 +1,9 @@
+package bit.hawkhje1.locationteleporterrand;
+
+public class GeoPluginMaxIterationsException extends Exception {
+
+    public GeoPluginMaxIterationsException(String s){
+        super(s);
+    }
+
+}
--- LocationTeleporterRand/app/src/main/java/bit/hawkhje1/locationteleporterrand/MainActivity.java
+++ LocationTeleporterRand/app/src/main/java/bit/hawkhje1/locationteleporterrand/MainActivity.java
@@ -8,14 +8,16 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.util.Random;
-import java.util.concurrent.ExecutionException;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -57,29 +59,34 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public void onClick(View v) {
 
-            Coordinates teleportationLocation = getLocation();
-
-            double longitude = teleportationLocation.getLongitude();
-            double latitude = teleportationLocation.getLatitude();
-
-            String longitudeText = Double.toString(longitude);
-            String latitudeText = Double.toString(latitude);
-
-            tvLongitude.setText(longitudeText);
-            tvLatitude.setText(latitudeText);
-
             GeoPluginAsyncTask geoPlugin = new GeoPluginAsyncTask();
             geoPlugin.execute();
 
             try {
-                Coordinates randCoordinates = geoPlugin.get();
-                tvLocation.setText(randCoordinates.toString());
-            } catch (InterruptedException e) {
+                // get the geoplugin information
+                GeoPluginInfo geoPluginInfo = geoPlugin.get();
+
+                // get the coordinates from geoplugin
+                Coordinates teleportationLocation = geoPluginInfo.getOriginalCoordinates();
+
+                // get individual coordinates from geoplugin
+                double longitude = teleportationLocation.getLongitude();
+                double latitude = teleportationLocation.getLatitude();
+
+                // parse the longitude and latitude values
+                String longitudeText = Double.toString(longitude);
+                String latitudeText = Double.toString(latitude);
+
+                // output geoplugin data to screen
+                tvLongitude.setText(longitudeText);
+                tvLatitude.setText(latitudeText);
+
+                // output location to screen
+                tvLocation.setText(String.format("Closest Location: %s, %s", geoPluginInfo.getPlace(), geoPluginInfo.getCountryCode()));
+
+            } catch (Exception e) {
                 e.printStackTrace();
-                tvLocation.setText(e.getMessage());
-            } catch (ExecutionException e) {
-                e.printStackTrace();
-                tvLocation.setText(e.getMessage());
+                Toast.makeText(MainActivity.this, e.getMessage(), Toast.LENGTH_LONG).show();
             }
         }
 
@@ -88,74 +95,117 @@ public class MainActivity extends AppCompatActivity {
     // get the location coordinates
     public Coordinates getLocation(){
 
-        Random rand = new Random();
+        // generate a random longitude location
+        double longitude = Globals.RandomDouble(Globals.MIN_LONGITUDE, Globals.MAX_LONGITUDE);
 
-        double longitude = rand.nextDouble() * (MAX_LONGITUDE - MIN_LONGITUDE) + MIN_LONGITUDE;
-        double latitude = rand.nextDouble() * (MAX_LATITUDE - MIN_LATITUDE) + MIN_LATITUDE;
+        // generate a random latitude location
+        double latitude = Globals.RandomDouble(Globals.MIN_LATITUDE, Globals.MAX_LATITUDE);
 
+        // return coordinates
         return new Coordinates(longitude, latitude);
+
     }
 
     // async task for GeoPlugin
-    public class GeoPluginAsyncTask extends AsyncTask<String, Integer, Coordinates>
+    public class GeoPluginAsyncTask extends AsyncTask<String, Integer, GeoPluginInfo>
     {
         private static final String GEOPLUGIN_ASYNCTASK = "GEOPLUGIN_ASYNC";
 
         @Override
-        protected Coordinates doInBackground(String... params) {
+        protected GeoPluginInfo doInBackground(String... params) {
 
-            String geoPluginContents;
-
-            // generate random coordinates
-            Coordinates randCoordinates = getLocation();
+            GeoPluginInfo geoPluginInfo = null;
 
             try {
 
-                // output geoplugin async task to log
-                Log.d(GEOPLUGIN_ASYNCTASK, "Checking Coordinates " + randCoordinates.toString());
+                String geoPluginContent;
+                Coordinates randCoordinates;
 
-                // format the URL
-                String formattedURL = String.format(Globals.GEOPLUGIN_URL, randCoordinates.getLatitude(), randCoordinates.getLongitude());
+                int geoPluginIterations = 0;
 
-                // create URL
-                URL geoPluginURL = new URL(formattedURL);
+                do {
 
-                // create an HTTP URL Connection
-                HttpURLConnection httpURLConnection = (HttpURLConnection)geoPluginURL.openConnection();
+                    // generate random coordinates
+                    randCoordinates = getLocation();
 
-                // create an input stream
-                InputStream inputStream = httpURLConnection.getInputStream();
+                    // output geoplugin async task to log
+                    Log.d(GEOPLUGIN_ASYNCTASK, "Checking Coordinates " + randCoordinates.toString());
 
-                // create input stream reader
-                InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+                    // format the URL
+                    String formattedURL = String.format(Globals.GEOPLUGIN_URL, randCoordinates.getLatitude(), randCoordinates.getLongitude());
 
-                // create buffered reader
-                BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+                    // create URL
+                    URL geoPluginURL = new URL(formattedURL);
 
-                // create a string builder to get content from geoplugin
-                StringBuilder content = new StringBuilder();
+                    // create an HTTP URL Connection
+                    HttpURLConnection httpURLConnection = (HttpURLConnection) geoPluginURL.openConnection();
 
-                // create empty string
-                String currentLine;
+                    // create an input stream
+                    InputStream inputStream = httpURLConnection.getInputStream();
 
-                // loop through each line that's returned
-                while((currentLine = bufferedReader.readLine()) != null){
-                    content.append(currentLine);
-                }
+                    // create input stream reader
+                    InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
 
+                    // create buffered reader
+                    BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
 
+                    // create a string builder to get content from geoplugin
+                    StringBuilder content = new StringBuilder();
 
-            } catch (java.io.IOException e) {
+                    // create empty string
+                    String currentLine;
+
+                    // loop through each line that's returned
+                    while ((currentLine = bufferedReader.readLine()) != null) {
+                        content.append(currentLine);
+                    }
+
+                    geoPluginContent = content.toString();
+                    Log.d(GEOPLUGIN_ASYNCTASK, "Content Received From GeoPlugin: " + geoPluginContent);
+
+                    geoPluginIterations++;
+
+                    if(geoPluginIterations == Globals.MAX_GEOPLUGIN_ITERATIONS) {
+                        throw new GeoPluginMaxIterationsException("Application Failed to Find a Location within the Specific Iterations");
+                    }
+
+                }while(geoPluginContent.equals("[[]]"));
+
+                // Create JSONObject from GeoPlugin API
+                JSONObject geoPluginJSON = new JSONObject(geoPluginContent);
+                Log.d(GEOPLUGIN_ASYNCTASK, "JSON Object Output" + geoPluginJSON.toString());
+
+                // =========================== Get Content from JSON ===============================
+                String place = geoPluginJSON.getString("geoplugin_place");
+                String countryCode = geoPluginJSON.getString("geoplugin_countryCode");
+                String region = geoPluginJSON.getString("geoplugin_region");
+                String regionAbbreviated = geoPluginJSON.getString("geoplugin_regionAbbreviated");
+
+                double longitude = geoPluginJSON.getDouble("geoplugin_longitude");
+                double latitude = geoPluginJSON.getDouble("geoplugin_latitude");
+                Coordinates coordinates = new Coordinates(longitude, latitude);
+
+                double distanceMiles = geoPluginJSON.getDouble("geoplugin_distanceMiles");
+                double distanceKilometers = geoPluginJSON.getDouble("geoplugin_distanceKilometers");
+                // =================================================================================
+
+                // Store retrieved content into geoplugin info class
+                geoPluginInfo = new GeoPluginInfo(
+                        randCoordinates, coordinates, place,
+                        countryCode, region, regionAbbreviated,
+                        distanceMiles, distanceKilometers);
+
+            } catch (java.io.IOException | JSONException | GeoPluginMaxIterationsException e) {
                 e.printStackTrace();
             }
 
-            return randCoordinates;
+            return geoPluginInfo;
         }
 
         @Override
-        protected void onPostExecute(Coordinates coordinates) {
+        protected void onPostExecute(GeoPluginInfo geoPluginInfo) {
 
-            super.onPostExecute(coordinates);
+            super.onPostExecute(geoPluginInfo);
         }
     }
 
