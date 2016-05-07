e0d76c0471b60d8170f2f44ca0d3ef0e394b16df
Joshua Hawkhead <joshua.hawkhead@gmail.com>
2016-05-07 17:10:22 +1200

    Init Random Location Teleporter

---
+++ LocationTeleporterRand/app/src/main/java/bit/hawkhje1/locationteleporterrand/MainActivity.java
@@ -0,0 +1,13 @@
+package bit.hawkhje1.locationteleporterrand;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
