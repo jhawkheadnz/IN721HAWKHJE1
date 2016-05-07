23a641614b31b83db3d833a30fc15e3dfb2cb9da
Joshua Hawkhead <joshua.hawkhead@gmail.com>
2016-05-07 18:49:32 +1200

    Created References to View

--- LocationTeleporterRand/app/src/main/java/bit/hawkhje1/locationteleporterrand/MainActivity.java
+++ LocationTeleporterRand/app/src/main/java/bit/hawkhje1/locationteleporterrand/MainActivity.java
@@ -2,12 +2,47 @@ package bit.hawkhje1.locationteleporterrand;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView tvLatitude;
+    private TextView tvLocation;
+    private TextView tvLongitude;
+    private ImageView imgLocation;
+    private Button btnTeleport;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // setup textviews
+        tvLatitude = (TextView)findViewById(R.id.tvLatitudeValue);
+        tvLongitude = (TextView)findViewById(R.id.tvLongitudeValue);
+        tvLocation = (TextView)findViewById(R.id.tvLocation);
+
+        // setup image view
+        imgLocation = (ImageView)findViewById(R.id.imgLocation);
+
+        // setup button
+        btnTeleport = (Button)findViewById(R.id.btnGenerateLocation);
+
+        // attach button click handler
+        BtnClickTeleportHandler btnClickTeleportHandler = new BtnClickTeleportHandler();
+        btnTeleport.setOnClickListener(btnClickTeleportHandler);
+
+    }
+
+    // button onclick handler
+    public class BtnClickTeleportHandler implements View.OnClickListener {
+
+        @Override
+        public void onClick(View v) {
+            
+        }
     }
 }
