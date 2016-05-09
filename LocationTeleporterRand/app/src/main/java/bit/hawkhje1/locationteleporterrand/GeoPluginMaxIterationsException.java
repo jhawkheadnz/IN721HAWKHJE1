293ca557659e03b3e93b23289e666d75451f6629
Joshua Hawkhead <joshua.hawkhead@gmail.com>
2016-05-09 21:32:15 +1200

    Updated Completed Application

--- LocationTeleporterRand/app/src/main/java/bit/hawkhje1/locationteleporterrand/GeoPluginMaxIterationsException.java
+++
@@ -1,9 +0,0 @@
-package bit.hawkhje1.locationteleporterrand;
-
-public class GeoPluginMaxIterationsException extends Exception {
-
-    public GeoPluginMaxIterationsException(String s){
-        super(s);
-    }
-
-}
